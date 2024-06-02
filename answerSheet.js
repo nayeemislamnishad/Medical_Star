5c2aca33ac27cc14ee9b1370ce8623401e28e1dc
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-06-02 16:29:37 +0600

    Update answerSheet.js

--- answerSheet.js
+++ answerSheet.js
@@ -3,4 +3,4 @@ const gucco2="accabaaaadaccabdaabdbcababcadcdbaba";
 const gucco3="cabaadbadbaccbdaadbbbcabddaca";
 const gucco4="adadabaaabbbcabbacdacadbadbdca";
 const gucco5="abdaaccbabccdbaaabbbdbdcccccbb";
-const gucco6="adbccccadcbbcddacacbacbac";
+const gucco6="cbcdbcbaacabbadcdbddccbaababaccbdbbbabca";
