4551d1c4d2834aa9b7a892829810e991a333255f
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-06-02 16:25:04 +0600

    Update answerSheet.js

--- answerSheet.js
+++ answerSheet.js
@@ -2,5 +2,5 @@ const gucco1="cdbbbcbdddbbcbacaabcbcaaaabbdc";
 const gucco2="accabaaaadaccabdaabdbcababcadcdbaba";
 const gucco3="cabaadbadbaccbdaadbbbcabddaca";
 const gucco4="adadabaaabbbcabbacdacadbadbdca";
-const gucco5="ababbb";
+const gucco5="abdaaccbabccdbaaabbbdbdcccccbb";
 const gucco6="adbccccadcbbcddacacbacbac";
